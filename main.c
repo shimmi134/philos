632e7baf04ea0c83c826d348b6c1b5eb2fc56b9a
Sam <sami.himi06@gmail.com>
2025-08-23 11:10:28 +0200

    Need to do main loop

--- main.c
+++ main.c
@@ -1,17 +1,67 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <pthread.h>
+#include "philos.h"
 
-void give_forks(t_philos **p, int num)
+int	ft_atoi(const char *nptr)
 {
-	int i = 0
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+int	philo_sleep()
+{
+	return (0);
+}
+
+int	philo_eat()
+{
+	return (0);
+}
+
+void	ft_lonely_philo(t_philo *p)
+{
+
+}
+
+
+void	*philo_loop(void *arg)
+{
+	t_philo	*p;
+
+	p = (t_philo *)arg;
+	
+	if (p->rules->n_philos == 1)
+		return (ft_lonely_philo(p), NULL);
+
+	return (NULL);
+}
+
+void give_forks(t_philo **p, int num)
+{
+	int i = 0;
 	for (; i < num; ){
 		if (i == 0)
-		{
 			(p[i])->l_fork = &((p[num-1])->r_fork);
-		}
 		else
 			(p[i])->l_fork = &((p[i-1])->r_fork);
+		pthread_create(&(p[i])->thread, NULL, philo_loop,p[i]);
 		i++;
 	}
 }
@@ -31,6 +81,7 @@ t_rules *create_rules(int ac, char **av)
 		rules->must_eat = ft_atoi(av[5]);
 	else
 		rules->must_eat = -1;
+	rules->die = 0;
 	pthread_mutex_init(&(rules->mutex),NULL);
 	return (rules);
 }
@@ -39,21 +90,22 @@ t_philo** create_philos(t_rules *rules)
 {
 	t_philo **philos;
 
+	pthread_mutex_lock(&(rules->mutex));
 	philos = malloc(sizeof(t_philo *) * rules->n_philos);
 	if (!philos)
 		return (NULL);
-	pthread_mutex_lock(&(rules->mutex));
-	for (int i = 0; i < rules->n_philos, i++){	
-		philos[i] = malloc(sizeof(t_philo));
+	for (int i = 0; i < rules->n_philos; i++){	
+		philos[i] = malloc(sizeof(t_philo*));
 		(philos[i])->num = i+1;
 		(philos[i])->times_eaten = 0;
-		(philos[i])->time = gettime();
+		(philos[i])->time = 0;//gettime();
 		(philos[i])->rules = rules;
+		(philos[i])->died = 0;
 		pthread_mutex_init(&((philos[i])->r_fork), NULL);
 	}
 	give_forks(philos, rules->n_philos);
 	pthread_mutex_unlock(&(rules->mutex));
-
+	return (philos);
 }
 
 int main(int ac, char *av[])
@@ -73,5 +125,7 @@ int main(int ac, char *av[])
 		printf("Error creating rules, try again.\n");
 		return (2);
 	}
+	philos = create_philos(rules);
+	
 
 }
--- philos.h
+++ philos.h
@@ -2,29 +2,33 @@
 #define PHILOS_H
 
 #include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_rule
+{
+	int				eat_time;
+	int				death_time;
+	int				sleep_time;
+	int				must_eat;
+	int				start_time;
+	int				n_philos;
+	int				die;
+	pthread_mutex_t	mutex;
+}		t_rules;
 
 typedef struct s_philo
 {
 	int				num;
 	int				times_eaten;
+	int				died;
 	long			time;
 	t_rules			*rules;
+	pthread_t		thread;
 	pthread_mutex_t *l_fork;	//fork you steal from the one on your left
 	pthread_mutex_t r_fork;		//Your own fork
-	
-
 }		t_philo;
-
-typedef struct s_rule
-{
-	int eat_time;
-	int	death_time;
-	int	sleep_time;
-	int must_eat;
-	int start_time;
-	int n_philos;
-	pthread_mutex_t mutex;
-}	t_rules;
 
 
 #endif
