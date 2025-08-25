b8b0cde9c7777c63e17537dfa13ddb18943bffb4
Sami Himi Ben Alilou <shimi-be@cbr6s5.42barcelona.com>
2025-08-25 19:46:22 +0200

    done with sleep and think

--- main.c
+++ main.c
@@ -25,30 +25,115 @@ int	ft_atoi(const char *nptr)
 	return (result * sign);
 }
 
-int	philo_sleep()
+long	get_time()
 {
-	return (0);
+	struct timeval tv;
+	static time_t start_time;
+
+	if (start_time == 0)
+	{
+		gettimeofday(&tv, NULL);
+		start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	}
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec/1000) - start_time);
 }
 
-int	philo_eat()
+
+int	philo_eat(t_philo *philo)
 {
+	long time;
+
+
+	pthread_mutex_lock(&(philo->r_fork));
+
+	pthread_mutex_lock(philo->l_fork);
+	pthread_mutex_lock(&(philo->rules->mutex));
+	philo->time = get_time();
+	printf("%ld %i has eaten\n", philo->time, philo->num);
+	philo->times_eaten++;
+	pthread_mutex_unlock(&(philo->rules->mutex));
+	usleep(philo->sleep_time*1000);
+	pthread_mutex_unlock(&(philo->r_fork));
+	pthread_mutex_unlock(philo->l_fork);
 	return (0);
 }
 
 void	ft_lonely_philo(t_philo *p)
 {
+	pthread_mutex_lock(p->rules->mutex);
+	printf("%ld %i has taken a fork\n", get_time(), philo->num);
+	usleep(p->death_time*1000);
+	printf("%ld %i died\n", get_time(), p->num);
+	pthread_mutex_unlock(p->rules->mutex);
 
 }
 
+void	philo_think(t_philo *philo)
+{
+	pthread_mutex_lock(philo->rules->mutex);
+	if (get_time() - philo->time >= philo->rules->death_time)
+		return (philo_death(philo));
+	if (philo->rules->die != 0)
+		printf("%ld %d is thinking\n", gettime(), philo->num);
+	pthread_mutex_unlock(philo->rules->mutex);
+}
+
+void	philo_sleep(t_philo *philo)
+{
+	pthread_mutex_lock(philo->rules->mutex);
+	if (philo->rules->die != 0)
+		printf("%ld %i is sleeping\n", get_time(), philos->num);
+	usleep(philos->rules->sleep_time * 1000);
+	pthread_mutex_unlock(philo->rules->mutex);
+}
 
 void	*philo_loop(void *arg)
 {
-	t_philo	*p;
+	t_philo	*philo;
 
-	p = (t_philo *)arg;
+	philo = (t_philo *)arg;
 	
-	if (p->rules->n_philos == 1)
+	if (philo->rules->n_philos == 1)
 		return (ft_lonely_philo(p), NULL);
+	philo->time = get_time();
+	pthread_mutex_lock(philo->rules->mutex);
+	while (philo->rules->die == 0 && philo->times_eaten != philo->rules->must_eat)
+	{
+		pthread_mutex_unlock(philo->rules->mutex);
+		pthread_mutex_lock(philo->rules->mutex);
+		if (philo->died != 0|| philo->rules->die != 0)
+		{
+			if (philo->died)
+				philo_death(philo);
+			break;
+		}
+		pthread_mutex_unlock(philo->rules->mutex);
+		philo_eat(philo);
+		pthread_mutex_lock(philo->rules->mutex);
+		if (philo->times_eaten == philo->rules->must_eat)
+		{
+			philo_is_full(philo);
+			break ;
+		}
+		if (philo->died != 0|| philo->rules->die != 0)
+		{
+			if (philo->died)
+				philo_death(philo);
+			break;
+		}
+		pthread_mutex_unlock(philo->rules->mutex);
+		philo_sleep(philo);
+		pthread_mutex_lock(philo->rules->mutex);
+		if (philo->died != 0|| philo->rules->die != 0)
+		{
+			if (philo->died)
+				philo_death(philo);
+			break;
+		}
+		pthread_mutex_unlock(philo->rules->mutex);
+		philo_think(philo);
+	}
 
 	return (NULL);
 }
@@ -84,6 +169,20 @@ t_rules *create_rules(int ac, char **av)
 	rules->die = 0;
 	pthread_mutex_init(&(rules->mutex),NULL);
 	return (rules);
+}
+
+void	destroy_philos(t_philo **philos, int j)
+{
+	int		i;
+
+	i = 0;
+	while (i < j)
+	{
+		pthread_join((philos[i])->thread, NULL);
+		free(philos[i]);
+		i++;
+	}
+	free(philos);
 }
 
 t_philo** create_philos(t_rules *rules)
@@ -126,6 +225,7 @@ int main(int ac, char *av[])
 		return (2);
 	}
 	philos = create_philos(rules);
-	
-
+	destroy_philos(philos, rules->n_philos);
+	free(rules);
+	return (0);
 }
