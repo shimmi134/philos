79dbc8e09f52b8c360a11dea6bb74f532ce6770b
Sami Himi Ben Alilou <shimi-be@cbr12s3.42barcelona.com>
2025-08-26 19:49:04 +0200

    I need to make it so that odd and even philos grab the forks in a different order

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shimi-be <shimi-be@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 16:54:35 by shimi-be          #+#    #+#             */
+/*   Updated: 2025/08/26 16:54:37 by shimi-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philos.h"
+#include <sys/time.h>
 
 int	ft_atoi(const char *nptr)
 {
@@ -39,53 +52,58 @@ long	get_time()
 	return ((tv.tv_sec * 1000) + (tv.tv_usec/1000) - start_time);
 }
 
+void	philo_death(t_philo *philo)
+{
+	printf("%ld %i died\n", get_time(), philo->num);
+}
+
 
 int	philo_eat(t_philo *philo)
 {
-	long time;
-
-
-	pthread_mutex_lock(&(philo->r_fork));
-
+	if (get_time() - philo->time >= philo->rules->death_time)
+		return (philo_death(philo), 1);
+	pthread_mutex_lock(&(philo->r_fork));	
+	printf("%ld %i has taken a fork\n", get_time(), philo->num);
+	if (get_time() - philo->time >= philo->rules->death_time)
+		return (philo_death(philo), 1);
 	pthread_mutex_lock(philo->l_fork);
-	pthread_mutex_lock(&(philo->rules->mutex));
+	printf("%ld %i has taken a fork\n", get_time(), philo->num);
+	usleep(philo->rules->eat_time*1000);
 	philo->time = get_time();
 	printf("%ld %i has eaten\n", philo->time, philo->num);
-	philo->times_eaten++;
-	pthread_mutex_unlock(&(philo->rules->mutex));
-	usleep(philo->sleep_time*1000);
 	pthread_mutex_unlock(&(philo->r_fork));
 	pthread_mutex_unlock(philo->l_fork);
+	philo->times_eaten++;
 	return (0);
 }
 
-void	ft_lonely_philo(t_philo *p)
+void	ft_lonely_philo(t_philo *philo)
 {
-	pthread_mutex_lock(p->rules->mutex);
+	pthread_mutex_lock(&philo->rules->mutex);
 	printf("%ld %i has taken a fork\n", get_time(), philo->num);
-	usleep(p->death_time*1000);
-	printf("%ld %i died\n", get_time(), p->num);
-	pthread_mutex_unlock(p->rules->mutex);
+	usleep(philo->rules->death_time*1000);
+	printf("%ld %i died\n", get_time(), philo->num);
+	pthread_mutex_unlock(&philo->rules->mutex);
 
 }
 
 void	philo_think(t_philo *philo)
 {
-	pthread_mutex_lock(philo->rules->mutex);
+	pthread_mutex_lock(&philo->rules->mutex);
 	if (get_time() - philo->time >= philo->rules->death_time)
 		return (philo_death(philo));
 	if (philo->rules->die != 0)
-		printf("%ld %d is thinking\n", gettime(), philo->num);
-	pthread_mutex_unlock(philo->rules->mutex);
+		printf("%ld %d is thinking\n", get_time(), philo->num);
+	pthread_mutex_unlock(&philo->rules->mutex);
 }
 
 void	philo_sleep(t_philo *philo)
 {
-	pthread_mutex_lock(philo->rules->mutex);
+	pthread_mutex_lock(&philo->rules->mutex);
 	if (philo->rules->die != 0)
-		printf("%ld %i is sleeping\n", get_time(), philos->num);
-	usleep(philos->rules->sleep_time * 1000);
-	pthread_mutex_unlock(philo->rules->mutex);
+		printf("%ld %i is sleeping\n", get_time(), philo->num);
+	usleep(philo->rules->sleep_time * 1000);
+	pthread_mutex_unlock(&philo->rules->mutex);
 }
 
 void	*philo_loop(void *arg)
@@ -93,45 +111,47 @@ void	*philo_loop(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	
+	if (philo->num % 2 != 0)
+		usleep(50);
 	if (philo->rules->n_philos == 1)
-		return (ft_lonely_philo(p), NULL);
+		return (ft_lonely_philo(philo), NULL);
 	philo->time = get_time();
-	pthread_mutex_lock(philo->rules->mutex);
 	while (philo->rules->die == 0 && philo->times_eaten != philo->rules->must_eat)
 	{
-		pthread_mutex_unlock(philo->rules->mutex);
-		pthread_mutex_lock(philo->rules->mutex);
+		pthread_mutex_lock(&philo->rules->mutex);
 		if (philo->died != 0|| philo->rules->die != 0)
 		{
 			if (philo->died)
 				philo_death(philo);
+			pthread_mutex_unlock(&philo->rules->mutex);
 			break;
 		}
-		pthread_mutex_unlock(philo->rules->mutex);
+		pthread_mutex_unlock(&philo->rules->mutex);
 		philo_eat(philo);
-		pthread_mutex_lock(philo->rules->mutex);
+		pthread_mutex_lock(&philo->rules->mutex);
 		if (philo->times_eaten == philo->rules->must_eat)
 		{
-			philo_is_full(philo);
+			//philo_is_full(philo);
 			break ;
 		}
 		if (philo->died != 0|| philo->rules->die != 0)
 		{
 			if (philo->died)
 				philo_death(philo);
+			pthread_mutex_unlock(&philo->rules->mutex);
 			break;
 		}
-		pthread_mutex_unlock(philo->rules->mutex);
+		pthread_mutex_unlock(&philo->rules->mutex);
 		philo_sleep(philo);
-		pthread_mutex_lock(philo->rules->mutex);
+		pthread_mutex_lock(&philo->rules->mutex);
 		if (philo->died != 0|| philo->rules->die != 0)
 		{
 			if (philo->died)
 				philo_death(philo);
+			pthread_mutex_unlock(&philo->rules->mutex);
 			break;
 		}
-		pthread_mutex_unlock(philo->rules->mutex);
+		pthread_mutex_unlock(&philo->rules->mutex);
 		philo_think(philo);
 	}
 
@@ -151,7 +171,7 @@ void give_forks(t_philo **p, int num)
 	}
 }
 
-t_rules *create_rules(int ac, char **av)
+t_rules *create_rules(char **av)
 {
 	t_rules *rules;
 
@@ -194,7 +214,7 @@ t_philo** create_philos(t_rules *rules)
 	if (!philos)
 		return (NULL);
 	for (int i = 0; i < rules->n_philos; i++){	
-		philos[i] = malloc(sizeof(t_philo*));
+		philos[i] = malloc(sizeof(t_philo));
 		(philos[i])->num = i+1;
 		(philos[i])->times_eaten = 0;
 		(philos[i])->time = 0;//gettime();
@@ -202,6 +222,7 @@ t_philo** create_philos(t_rules *rules)
 		(philos[i])->died = 0;
 		pthread_mutex_init(&((philos[i])->r_fork), NULL);
 	}
+	printf("Gothere\n");
 	give_forks(philos, rules->n_philos);
 	pthread_mutex_unlock(&(rules->mutex));
 	return (philos);
@@ -209,7 +230,6 @@ t_philo** create_philos(t_rules *rules)
 
 int main(int ac, char *av[])
 {
-	pthread_t	*t;
 	t_rules		*rules;
 	t_philo		**philos;
 
@@ -218,7 +238,7 @@ int main(int ac, char *av[])
 		printf("Wrong argument number, only 4 or 5!\n");
 		return (1);
 	}
-	rules = create_rules(ac, av);
+	rules = create_rules(av);
 	if (!rules)
 	{
 		printf("Error creating rules, try again.\n");
